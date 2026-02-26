f4a06f48308e114b4e34aa16d236d58fde6dd577
connordfelt <connor.d.felt@gmail.com>
2026-02-26 09:20:20 -0700

    Updated quotes
    Changed quotes to reflect in game world

--- yee-haw/data/Text Quotes/death_quote_scripte.gd
+++ yee-haw/data/Text Quotes/death_quote_scripte.gd
@@ -2,10 +2,10 @@
 extends VBoxContainer
 
 var quotes = [
-	"Failure is just practice.",
-	"Try again, hero.",
-	"Even legends fall.",
-    "You’ll get it next time."
+	"We wouldn't be here if Hard Light was easy to eradicate.",
+	"Well, I didn't get my money's worth.",
+	"The Core still lives. We can't give up.",
+    "The Chemists can't be right...they can't."
 ]
 
 func _ready():
