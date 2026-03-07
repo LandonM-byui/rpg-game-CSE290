04979c7d0eb727a0a1eeb64ef4d013a3ab00ad1f
LandonM-byui <mah22009@byui.edu>
2026-03-06 18:14:06 -0700

    Player movement implimented

--- yee-haw/modules/PlayerCharactersArea/player_character.gd
+++ yee-haw/modules/PlayerCharactersArea/player_character.gd
@@ -1,13 +1,25 @@
 extends Node2D
+signal player_clicked(pos)
 
 var player_id = 0
-var player_position = Vector2(0,0)
+var char_name = ""
+var player_position = []
 var player_health = 10
 var current = false
 
-func initialize(id: int, current: bool) -> void:
+func initialize(id: int, name: String, in_party: bool, row: int, column: int) -> void:
 	player_id = id;
-	current = current;
+	current = in_party;
+	char_name = name;
+	player_position.append(row)
+	player_position.append(column)
 	
-func _set_player_position(pos: Vector2) -> void:
-	player_position = pos
+func _set_player_position(row: int, column: int) -> void:
+	player_position.clear()
+	player_position.append(row)
+	player_position.append(column)
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		player_clicked.emit(player_position)
